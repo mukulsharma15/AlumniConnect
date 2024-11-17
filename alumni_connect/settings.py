6e909ec7df6a64ac9a467cdd3dbf5d88b51295f8
mukulbamn <mlegend9070@gmail.com>
2024-11-17 21:56:28 +0530

    final changes, added bhoomi's home and team page

--- alumni_connect/settings.py
+++ alumni_connect/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'alumni_connect.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_2_ac',  # Your RDS DB name
+        'NAME': 'AlumniConnect',  # Your RDS DB name
         'USER': 'admin',
         'PASSWORD': 'zuqveb-gihgyh-xaFbu3',
         'HOST': 'alumniconnect-db.c7w2mqyuejv6.eu-north-1.rds.amazonaws.com',  # RDS endpoint
