83d7f4d8bb02b18974dccdcddf9a15dbce0c57cc
mukulbamn <mlegend9070@gmail.com>
2024-11-18 00:25:49 +0530

    bugs fixed

--- alumni_connect/settings.py
+++ alumni_connect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7pyrtgz3pv!s0go#kut996%+xjwcwhsn*q*b=1no(mk#j)5))j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*']
 
 # Application definition
 
